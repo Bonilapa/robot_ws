fb2cf05ceca603927996f96a9619bf966506a100
Akkey <a.astakhov@innopolis.ru>
2020-09-10 04:56:55 -0700

    creating workspace

---
+++ build/cart_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bonilapa/robot_ws/src/cart_robot
+# Build directory: /home/bonilapa/robot_ws/build/cart_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
